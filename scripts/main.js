9821ed74a1583d6eea906b468160b4c269936ebc
Ben Walpole <benwalpole95@gmail.com>
2020-07-09 14:37:50 +0100

    Minor change just to test git

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let button = document.querySelector('button');
 
 button.onclick = function() {
-    alert('CONGRATSSSSS YOU ARREE CORRRRRECTTTTT!!!!!!!')
+    alert('CONGRATSSSSS YOU ARREE CORRRRRECTTTTT!!11!!!1!!')
 }
